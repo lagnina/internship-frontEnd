cafa68a3470d4a633fd9932d7806434ba07e5074
Walid Tazi <walidtazi14@gmail.com>
2020-10-02 02:25:39 +0100

    confirm password

--- src/app/post-resume/post-resume.component.ts
+++ src/app/post-resume/post-resume.component.ts
@@ -48,11 +48,14 @@ export class PostResumeComponent implements OnInit {
     this.userForm = this.fb.group({
       username: [''],
       password: [''],
+      confirmPassword: ['', Validators.required],
       role: ['0'],
       stagiaire: this.stagiaireForm,
       entreprise:this.entrepriseForm
 
-    });
+    }, {
+      validator: this.MustMatch('password', 'confirmPassword')
+  });
     this.userForm.valueChanges.subscribe(()=>console.log(this.userForm.value));
   }
 
@@ -69,6 +72,24 @@ export class PostResumeComponent implements OnInit {
 
   }
   
+MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+
+        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            // return if another validator has already found an error on the matchingControl
+            return;
+        }
+
+        // set error on matchingControl if validation fails
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
+}
   
 
 }
